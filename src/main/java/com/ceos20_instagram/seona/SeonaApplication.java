a66c485b39ca34aececd5ed9089459ef73d6fc41
seona-moon <fhflwhwl5@gmail.com>
2024-09-20 17:38:56 +0900

    chore : 프로젝트 초기 설정

---
+++ src/main/java/com/ceos20_instagram/seona/SeonaApplication.java
@@ -0,0 +1,13 @@
+package com.ceos20_instagram.seona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceos20_instagram/seona/SeonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos20_instagram.seona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
